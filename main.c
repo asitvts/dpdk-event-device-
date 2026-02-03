37f642e91e81084c56faf24c0d8e4f4e89d8157a
asitvts <asitvts@gmail.com>
2026-02-03 10:06:22 +0530

    packet receive and transmit port and queues linked

--- main.c
+++ main.c
@@ -1,127 +1,156 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 
 #include <rte_eal.h>
 #include <rte_eventdev.h>
+#include <rte_ethdev.h>
 
 #define DEV_ID 0
-#define CONS_PORT_ID 1
+
+#define V4_Q_ID 0
+#define V6_Q_ID 1
+
 #define PROD_PORT_ID 0
+#define IPV4_CONS_PORT_ID 1
+#define IPV6_CONS_PORT_ID 2
+
 
 int main(int argc, char** argv){
-    int ret = rte_eal_init(argc,argv);
+
+    int ret;
+    ret = rte_eal_init(argc, argv);
 
     if(ret<0){
-        printf("rte_eal_init failed \n");
+        printf("rte_eal_init failed\n");
         return 1;
     }
 
-    printf("eal initialised\n");
+    printf("initialization done\n");
 
-    struct rte_event_dev_config event_config={0};
-    event_config.nb_event_ports=2;
-    event_config.nb_event_queues=1;
-    event_config.nb_event_port_dequeue_depth=32;
-    event_config.nb_event_port_enqueue_depth=32;
-    event_config.nb_event_queue_flows=1024;
-    event_config.nb_events_limit=4096;
 
-    ret = rte_event_dev_configure(DEV_ID, &event_config);
+
+    // event device config
+    struct rte_event_dev_config econfig={0};
+    econfig.nb_event_ports=3;
+    econfig.nb_event_queues=2;
+    econfig.nb_event_port_dequeue_depth=32;
+    econfig.nb_event_port_enqueue_depth=32;
+    econfig.nb_event_queue_flows=1;     // each packet will have sche type as parallel anyway
+
+    ret = rte_event_dev_configure(DEV_ID, &econfig);
     if(ret<0){
-        printf("event dev configure failed\n");
+        printf("event device configuratopn failed\n");
         return 2;
     }
 
-    printf("event device configured\n");
+    printf("event device configuration done\n");
 
-    struct rte_event_queue_conf queue_config={0};
-    queue_config.schedule_type=RTE_SCHED_TYPE_ATOMIC;
-    queue_config.nb_atomic_flows=1024;
-    queue_config.nb_atomic_order_sequences=1024;
-    queue_config.priority=RTE_EVENT_DEV_PRIORITY_NORMAL;
 
-    ret = rte_event_queue_setup(DEV_ID, 0, &queue_config);
+
+
+
+    // queue setup
+    struct rte_event_queue_conf ipv4queue={0};
+    ipv4queue.schedule_type = RTE_SCHED_TYPE_PARALLEL;
+    ret=rte_event_queue_setup(DEV_ID, V4_Q_ID, &ipv4queue);
     if(ret<0){
-        printf("queue setup failed\n");
+        printf("error while queue setup for ipv4 queue\n");
+    }
+
+    struct rte_event_queue_conf ipv6queue={0};
+    ipv6queue.schedule_type = RTE_SCHED_TYPE_PARALLEL;
+    ret=rte_event_queue_setup(DEV_ID, V6_Q_ID, &ipv6queue);
+    if(ret<0){
+        printf("error while queue setup for ipv6 queue\n");
+    }
+    printf("queue setup done\n");
+
+
+
+
+
+
+    // port setup
+    struct rte_event_port_conf receive_port;
+    ret=rte_event_port_default_conf_get(DEV_ID, PROD_PORT_ID, &receive_port);
+    if(ret<0){
+        printf("error fetching default config for port receive\n");
+    }
+    receive_port.event_port_cfg= RTE_EVENT_PORT_CFG_HINT_PRODUCER;
+
+    struct rte_event_port_conf ipv4_port;
+    ret=rte_event_port_default_conf_get(DEV_ID, IPV4_CONS_PORT_ID, &ipv4_port);
+    if(ret<0){
+        printf("error fetching default config for port ipv4\n");
+    }
+    ipv4_port.event_port_cfg= RTE_EVENT_PORT_CFG_HINT_CONSUMER;
+
+    struct rte_event_port_conf ipv6_port;
+    ret=rte_event_port_default_conf_get(DEV_ID, IPV6_CONS_PORT_ID, &ipv6_port);
+    if(ret<0){
+        printf("error fetching default config for port ipv6\n");
+    }
+    ipv6_port.event_port_cfg= RTE_EVENT_PORT_CFG_HINT_CONSUMER;
+
+    ret=rte_event_port_setup(DEV_ID, PROD_PORT_ID, &receive_port);
+    if(ret<0){
+        printf("receive port setup failed\n");
         return 3;
     }
 
-    printf("queue created\n");
-
-
-    struct rte_event_port_conf port_config;
-
-    rte_event_port_default_conf_get(DEV_ID, 0, &port_config);
-
-    port_config.event_port_cfg=RTE_EVENT_PORT_CFG_HINT_PRODUCER;
-    ret= rte_event_port_setup(DEV_ID, PROD_PORT_ID, &port_config);
+    ret=rte_event_port_setup(DEV_ID, IPV4_CONS_PORT_ID, &ipv4_port);
     if(ret<0){
-        printf("port setup failed for port 0 which is producer\n");
+        printf("ipv4 port setup failed\n");
         return 4;
     }
 
-    port_config.event_port_cfg=RTE_EVENT_PORT_CFG_HINT_CONSUMER;
-    ret= rte_event_port_setup(DEV_ID, CONS_PORT_ID, &port_config);
+    ret=rte_event_port_setup(DEV_ID, IPV6_CONS_PORT_ID, &ipv6_port);
     if(ret<0){
-        printf("port setup failed for port 1 which is consumer\n");
-        return 4;
+        printf("ipv port setup failed\n");
+        return 5;
     }
-
     printf("ports created\n");
 
 
-    // link consumer port to queue
-    uint8_t queues[]={0};
-    uint8_t priorities[]={RTE_EVENT_DEV_PRIORITY_NORMAL};
-    ret=rte_event_port_link(DEV_ID, CONS_PORT_ID, queues, priorities, 1);
-    if(ret<1){
-        printf("number of links established is less than 1\n");
-    }
 
-    ret=rte_event_dev_start(DEV_ID);
+
+
+
+
+    //port linking
+    uint8_t ipv4queues[]={V4_Q_ID};
+    uint8_t prio[]={RTE_EVENT_DEV_PRIORITY_NORMAL};
+    ret=rte_event_port_link(DEV_ID, IPV4_CONS_PORT_ID, ipv4queues, prio, 1);
     if(ret<0){
-        printf("device has not started properly\n");
-        return 5;
+        printf("ipv4 port linking failed\n");
     }
 
-    printf("event device started\n");
-
-
-    struct rte_event event={0};
-    // how to schedule event
-    event.queue_id=0;
-    event.op=RTE_EVENT_OP_NEW;
-    event.sched_type=RTE_SCHED_TYPE_ATOMIC;
-    event.event_type=RTE_EVENT_TYPE_CPU;
-    event.flow_id=1;
-    // payload
-    event.u64=12345;
-
-    printf("enqueue event\n");
-
-    while(rte_event_enqueue_burst(DEV_ID, PROD_PORT_ID, &event, 1)!=1){
-        printf("number of events actually enqueued are less than 1\n");
-        rte_pause();
+    uint8_t ipv6queues[]={V6_Q_ID};
+    prio[0]=RTE_EVENT_DEV_PRIORITY_LOWEST;
+    ret=rte_event_port_link(DEV_ID, IPV6_CONS_PORT_ID, ipv6queues, prio, 1);
+    if(ret<0){
+        printf("ipv6 port linking failed\n");
     }
 
+    printf("both ports linked\n");
 
-    struct rte_event out={0};
 
-    printf("waiting for event\n");
 
-    while(1){
-        ret=rte_event_dequeue_burst(DEV_ID, CONS_PORT_ID, &out, 1, 0);
-        if(ret){
-            printf("received event data: %ld\n", out.u64);
-            break;
-        }
+
+
+    // device start
+    ret= rte_event_dev_start(DEV_ID);
+    if(ret<0){
+        printf("event device start failed\n");
+        return 6;
     }
 
-    rte_event_dev_stop(DEV_ID);
-    rte_event_dev_close(DEV_ID);
+    printf("device started\n");
 
-    printf("event device stopped and closed\n");
+
+    
+
 
 
     return 0;
